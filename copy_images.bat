ae8fa24f3736812ae6665cb8886075b3ff3c9a5a
Neyred576 <prospermwendwa2006@gmail.com>
2026-07-22 14:17:44 +0300

    Add division images, Business Solutions page, update nav links

--- copy_images.bat
+++ copy_images.bat
@@ -1,7 +1,15 @@
 @echo off
-robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Divisions" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Divisions" /E /NFL /NDL /NJH /NJS
-robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Portfolio & Achivements" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Portfolio & Achivements" /E /NFL /NDL /NJH /NJS
-robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Social Responsibilities" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Social Responsibilities" /E /NFL /NDL /NJH /NJS
-echo.
-echo ALL IMAGES COPIED SUCCESSFULLY!
+echo Creating Generated folder...
+mkdir "c:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Generated" 2>nul
+
+echo Copying REEL Power Image...
+copy "C:\Users\prosp\.gemini\antigravity-ide\brain\e82ff927-857d-4f66-ab2a-3dd0aafc8e64\reel_power_delivery_1784718483267.png" "c:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Generated\reel_power_delivery.png" /Y
+
+echo Copying REEL Branding Image...
+copy "C:\Users\prosp\.gemini\antigravity-ide\brain\e82ff927-857d-4f66-ab2a-3dd0aafc8e64\reel_branding_offer_1784718520702.png" "c:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Generated\reel_branding_offer.png" /Y
+
+echo Copying REEL Smart Image...
+copy "C:\Users\prosp\.gemini\antigravity-ide\brain\e82ff927-857d-4f66-ab2a-3dd0aafc8e64\reel_smart_catalogue_1784718554762.png" "c:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Generated\reel_smart_catalogue.png" /Y
+
+echo Done! You can now view the updated pages.
 pause
