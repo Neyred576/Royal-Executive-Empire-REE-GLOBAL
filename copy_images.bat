91c647c2373b39971e950ce8c3b299b082134790
Neyred576 <prospermwendwa2006@gmail.com>
2026-07-22 12:08:58 +0300

    feat: add real images for divisions, portfolio, and CSR sections

---
+++ copy_images.bat
@@ -0,0 +1,7 @@
+@echo off
+robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Divisions" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Divisions" /E /NFL /NDL /NJH /NJS
+robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Portfolio & Achivements" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Portfolio & Achivements" /E /NFL /NDL /NJH /NJS
+robocopy "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Images\Social Responsibilities" "C:\Users\prosp\Desktop\WEBSITES\Royal Exercutive Empire\Web\Images\Social Responsibilities" /E /NFL /NDL /NJH /NJS
+echo.
+echo ALL IMAGES COPIED SUCCESSFULLY!
+pause
